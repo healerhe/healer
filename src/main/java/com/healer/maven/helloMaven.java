b812c3c59216ba3eed4c130a9ea246e9f945ee51
healerhe <1911978311@qq.com>
2018-08-09 09:20:29 +0800

    test

---
+++ src/main/java/com/healer/maven/helloMaven.java
@@ -0,0 +1,21 @@
+package com.healer.maven;
+
+/**
+ * @ description: 测试类
+ * @ author: healer
+ * @ create: 2018/8/8
+ **/
+public class helloMaven {
+
+    /**
+     * @ Description:
+     * @ Param: [name]
+     * @ return: java.lang.String
+     * @ throw:
+     * @ Author: healer
+     * @ Date: 2018-08-08
+     */
+    public String sayHello(String name){
+        return "hello"+name;
+    }
+}
---
+++ src/test/java/com/healer/maven/testHelloMaven.java
@@ -0,0 +1,31 @@
+package com.healer.maven;
+
+import org.junit.Test;
+
+/**
+ * @description:
+ * @author: healer
+ * @create: 2018/8/8
+ **/
+public class testHelloMaven {
+
+    @Test
+    /**
+     * @Description:
+     * @Param: []
+     * @return: void
+     * @throw:
+     * @Author: healer
+     * @Date: 2018-08-08
+     */
+    public void test(){
+        /*String name = "healer";
+        helloMaven h = new helloMaven();
+        System.out.println(h.sayHello(name));*/
+
+        String name = "healer";
+        helloMaven h = new helloMaven();
+        System.out.println(h.sayHello(name));
+
+    }
+}
